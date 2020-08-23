f0eadbbbf2d491b93e5a401299a928eacf1feda6
Alejandro Pacheco <alepacheco99@gmail.com>
2020-08-23 15:40:21 +0000

    Add healthpage

---
+++ pages/healthcheck/[user].tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Page = styled.div`
+    margin-top: 10vh;
+    font-family: monospace;
+    text-align: center;
+`;
+const Index = () => {
+
+    return (
+        <Page>
+      <div>Health check done for user x</div>
+        </Page>
+    )
+}
+
+
+export default Index;
